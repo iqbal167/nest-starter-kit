a2325de9e3da6e4fad9c42a6cf49790e0198cafd
iqbal167 <muhammadiqbalali167@gmail.com>
2022-12-26 14:40:31 +0700

    refactor: auth guard uses

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,10 @@
 import { Controller, Req, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('authentications')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('local')
   async login(@Req() req: Request) {
     return req.user;
